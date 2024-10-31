c7920be9cf799a2e15cd6e619bee20a26a981b43
Domingos Rodrigues <domingos.rodrigues@inventvision.com.br>
2024-10-30 21:14:46 -0300

    fix: :bug: Fix ability of Prefect training task to save runs; ClearML integrated

--- prefect-ml-example2/train_yoloflow.py
+++ prefect-ml-example2/train_yoloflow.py
@@ -1,12 +1,26 @@
+import datetime
 import os
 from pathlib import Path
 from typing import List
 
 import pandas as pd
+import torch
 from PIL import Image
+import prefect
 from prefect import flow, task
+# from prefect.client import Client
 from prefect.logging import get_run_logger
 from sklearn.model_selection import train_test_split
+
+from ultralytics import YOLO
+
+script_path = os.path.abspath(__file__)
+WORKING_DIR = os.path.dirname(script_path)
+
+
+def generate_flow_run_name():
+    date = datetime.datetime.now(datetime.timezone.utc)
+    return f"yolo_train-{date:%Y-%m-%dT%H%M%S}"
 
 
 def get_bbox_image_slices(label_path: str):
@@ -49,8 +63,16 @@ def xywhn_to_bbox(yolo_bb, im_w, im_h):
 
 def move_images(image_list: List[str], crop_destdir: Path, split_name: str):
     for crop_src_relpath in image_list:
-        crop_dst_relpath = crop_src_relpath.replace("images", split_name)
+        crop_dst_relpath_str = crop_src_relpath.replace("images", split_name)
         crop_src_abspath = crop_destdir / crop_src_relpath
+
+        # flatten the folder tree structure under each class folder
+        crop_dst_relpath_parts = Path(crop_dst_relpath_str).parts
+        crop_dst_relpath_name = Path(crop_dst_relpath_str).name
+        crop_dst_relpath = Path(
+            crop_dst_relpath_parts[0], crop_dst_relpath_parts[1], crop_dst_relpath_name
+        )
+        # print(f"{crop_src_relpath} => {crop_dst_relpath}")
         crop_dst_abspath = crop_destdir / crop_dst_relpath
 
         # Ensure the destination directory exists
@@ -58,8 +80,21 @@ def move_images(image_list: List[str], crop_destdir: Path, split_name: str):
         os.rename(crop_src_abspath, crop_dst_abspath)
 
 
-@task(log_prints=True)
+@task(name="create_yolo_dataset", log_prints=True)
 def convert_dataset_to_yolo(dataset_inputdir: str, only_matched_classes=True):
+    """Generates a YOLO-formatted dataset from a time-accumulated
+        working dataset of images.
+
+    Arguments:
+        dataset_inputdir -- origin working directory of images and labels
+
+    Keyword Arguments:
+        only_matched_classes -- Builds a dataset of images with matching AI and human labels. (default: {True})
+
+    Returns:
+        Path to the YOLO dataset directory and a list of bounding box coordinates.
+    """
+
     logger = get_run_logger()
     logger.info(f"Reading data the dataset folder: '{dataset_inputdir}'")
 
@@ -74,12 +109,13 @@ def convert_dataset_to_yolo(dataset_inputdir: str, only_matched_classes=True):
     file_counts_per_class = df.groupby("ai_class").size().reset_index(name="file_count")
     file_total = file_counts_per_class["file_count"].sum()
     logger.info(file_counts_per_class)
-    logger.info(f"total number of files = {file_total}")
+    logger.info(f"Total number of truck images = {file_total}")
 
     logger.info("Create DATASET for YOLO CLASSIFICATION [cropping bounding boxes]")
     yolo_dataset_dir = Path(dataset_inputdir).parent / "gerdau_yolo_dataset"
+    # yolo_dataset_dir = Path(WORKING_DIR) / "gerdau_yolo_dataset"
     yolo_dataset_dir.mkdir(parents=True, exist_ok=True)
-    logger.info(f"yolo_dataset_dir = {yolo_dataset_dir}")
+    logger.info(f"YOLO dataset location: {yolo_dataset_dir}")
 
     # yolo_dataset_cropdir = yolo_dataset_dir / "images"
     # yolo_dataset_cropdir.mkdir(parents=True, exist_ok=True)
@@ -125,7 +161,7 @@ def convert_dataset_to_yolo(dataset_inputdir: str, only_matched_classes=True):
 
 @task(log_prints=True)
 def data_split(
-    crop_destdir: Path,
+    yolo_dsetdir: Path,
     crop_images_list: List,
     train_split_sz: float = 0.3,
     seed: int = 42,
@@ -140,14 +176,80 @@ def data_split(
     print(f"val_list: {len(val_list)}")
     print(f"test_list: {len(test_list)}")
 
-    move_images(train_list, crop_destdir, "train")
-    move_images(val_list, crop_destdir, "val")
-    move_images(test_list, crop_destdir, "test")
+    move_images(train_list, yolo_dsetdir, "train")
+    move_images(val_list, yolo_dsetdir, "val")
+    move_images(test_list, yolo_dsetdir, "test")
+
+    # create dataset.yaml to order the scrap classes during training
 
 
-@task
-def train_model(X_train, X_test, y_train):
-    pass
+@task(log_prints=True)
+def train_classification_model(
+    crop_dsetdir,
+    imgsz=224,
+    epochs=5,
+    project="automl-yolo-runs",
+    model_variant="yolo11m-cls",
+    amp=False,
+    val=True,
+):
+    """_summary_
+
+    Arguments:
+        crop_dsetdir -- _description_
+
+    Keyword Arguments:
+        imgsz -- _description_ (default: {224})
+        epochs -- _description_ (default: {5})
+        project -- _description_ (default: {"automl-yolo-runs"})
+        model_variant -- _description_ (default: {"yolo11m-cls"})
+        amp -- _description_ (default: {False})
+        val -- _description_ (default: {True})
+
+    Returns:
+        _description_
+    """
+    from clearml import Task
+
+    flow_run_id = prefect.runtime.flow_run.id
+
+    # create run folders
+    task_name = f"prefect_run_{flow_run_id[:8]}"
+    yolo_rundir = crop_dsetdir.parent / project
+    print(f"Flow run ID: {flow_run_id}")
+    print(f"Local storage for this run: {str(yolo_rundir)}")
+
+    # creating a ClearML Task
+    task = Task.init(
+        project_name = project,
+        task_name = task_name,
+        tags=[model_variant, "AutoML"]
+    )
+    task.set_parameter("model_variant", model_variant)
+
+    # load a pretrained model
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = YOLO(f"{model_variant}.pt").to(device)
+
+    args = dict(
+        data=crop_dsetdir,
+        imgsz=imgsz,
+        epochs=epochs,
+        project=str(yolo_rundir),
+        name=task_name,
+        amp=amp,
+        val=val,
+        close_mosaic=0,
+        label_smoothing=1
+    )
+
+    task.connect(args)
+
+    # Start training
+    print("Training started!")
+    model_results = model.train(**args)
+
+    return model_results, model
 
 
 @task
@@ -160,22 +262,53 @@ def evaluate_model(y_test, prediction: pd.DataFrame):
     pass
 
 
-@task
-def save_model(model):
-    pass
+@task(log_prints=True)
+def save_model(model_results, model):
+    """_summary_
+
+    Arguments:
+        model_results -- _description_
+        model -- _description_
+    """
+    print(f"model_results: {model_results}")
+
+    save_dir = getattr(model_results, "save_dir", None)
+    print(f"save_dir from model_results: {save_dir}")
+
+    print(f"model_info: {model.info()}")
+    # Save final model
+    model.save("auto_ml.pt")
+    print("Model saved as 'auto_ml.pt'")
 
 
-@flow(log_prints=True)
+@flow(
+    name="YOLOv11 classification training",
+    log_prints=True,
+    flow_run_name=generate_flow_run_name,
+)
 def yolo_workflow(dset_inputdir: str = ""):
-    crop_destdir, crop_images_list = convert_dataset_to_yolo(dset_inputdir)
-    data_split(crop_destdir, crop_images_list)
+    """_summary_
 
-    # prep_data = preprocessing(data)
-    # X_train, X_test, y_train, y_test = data_split(prep_data)
-    # model = train_model(X_train, X_test, y_train)
+    Keyword Arguments:
+        dset_inputdir -- _description_ (default: {""})
+    """
+    #######################################################################
+    logger = get_run_logger()
+    logger.info("Started YOLO classification training pipeline")
+    #######################################################################
+
+    yolo_dsetdir, crop_images_list = convert_dataset_to_yolo(dset_inputdir)
+
+    data_split(yolo_dsetdir, crop_images_list)
+
+    # this model fits on 'knuth' GPU
+    res, model = train_classification_model(yolo_dsetdir, model_variant="yolo11n-cls")
+
     # predictions = get_prediction(X_test, model)
+
     # evaluate_model(y_test, predictions)
-    # save_model(model)
+
+    save_model(res, model)
 
 
 if __name__ == "__main__":
